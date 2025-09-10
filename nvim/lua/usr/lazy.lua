2350e4ea1219e88c2f08081d1697f03d624e10b9
Xueshi Hu <xueshi.hu@smartx.com>
2025-09-10 20:08:06 +0800

    update

--- nvim/lua/usr/lazy.lua
+++ nvim/lua/usr/lazy.lua
@@ -37,7 +37,7 @@ require("lazy").setup({
     event = "BufReadPost",
     opts = {
       suggestion = {
-        enabled = not vim.g.ai_cmp,
+        enabled = false,
         auto_trigger = true,
         hide_during_completion = vim.g.ai_cmp,
         keymap = {
